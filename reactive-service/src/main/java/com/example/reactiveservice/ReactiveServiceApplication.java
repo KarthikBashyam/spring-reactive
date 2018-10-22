bcbea73597d4506de1ea300795f74187bb063ecb
Karthik <karthik@email.com>
2018-10-21 22:33:04 -0400

    Initial Commit

--- reactive-service/src/main/java/com/example/reactiveservice/ReactiveServiceApplication.java
+++ reactive-service/src/main/java/com/example/reactiveservice/ReactiveServiceApplication.java
@@ -1,12 +1,21 @@
 package com.example.reactiveservice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ReactiveServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReactiveServiceApplication.class, args);
+	}
+	
+	@Bean
+	CommandLineRunner startup() {
+		return args -> {
+			System.out.println("==== REACTIVE SERVICE STARTED ======");
+		};
 	}
 }
